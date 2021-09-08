3fdcb7ad98f38da6d600519ca0fbdee537a53282
Jerimiah Willhite <>
2021-09-07 20:01:42 -0700

    add comment/notes to main

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,7 +1,11 @@
+// package == project == workspace
+// Go has two packages: executable (go build) and reusable (code libraries)
+// package main == making executable package, anything else is for a reusable package
 package main
 
 import "fmt"
 
+// package main must have a main func
 func main() {
 	fmt.Println("Hello world!")
 }
