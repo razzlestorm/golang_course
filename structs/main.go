92bffadac9d4bb776af511b00715c5bf2ee883c0
Jerimiah Willhite <>
2021-09-16 22:07:49 -0700

    add comments about pointers

--- structs/main.go
+++ structs/main.go
@@ -34,10 +34,14 @@ func main() {
 	alexPointer.updateName("Alexis")
 	// %+v prints out all field names and their values
 	alex.print()
+	alex.updateName("AlexisNoPointer")
+	alex.print()
 }
 
 // *pointer = give me the value in the memory address this is pointing at
 // whenever you see a * where a type should be, it is type[]pointerToType
+
+// This function can also be called with both a pointer to person or a person type.
 func (pointerToPerson *person) updateName(newFirstName string) {
 	// Below is an operator that takes this pointer and turns it into an actual value.
 	(*pointerToPerson).firstName = newFirstName
@@ -45,7 +49,13 @@ func (pointerToPerson *person) updateName(newFirstName string) {
 
 // You can turn Address into Value with *Address
 // You can turn Value into Address with &Value
+// Slices don't need to worry about pointers, it's mainly structs?
 
 func (p person) print() {
 	fmt.Printf("%+v", p)
 }
+
+// When creating a slice, Go creates two separate data structures.
+// The first is a Slice (with pointer to Array head, capacity, and length), and then an internal array that represents all of the items.
+// use pointers to change the following in a function: int, float, string, bool, struct (these are value types)
+// pointers not necessary for: slices, maps, channels, pointers, functions (these are reference types)
